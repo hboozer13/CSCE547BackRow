11e2a0aea145c1ce052168b2b537e773e524b298
Nathan Russell <russellnathanielr@gmail.com>
2023-10-30 22:50:25 -0400

    new items

--- BackRowStore/Services/DataService.cs
+++ BackRowStore/Services/DataService.cs
@@ -16,12 +16,20 @@ namespace BackRowStore.Services
                 {"001", ("water bottle", 12.99, 11) },
                 {"002", ("apple", 0.99, 23) },
                 {"003", ("PS5", 499.99, 2) },
-                {"004", ("guitar", 159.99, 6) }
+                {"004", ("guitar", 159.99, 6) },
+                {"005", ("mr krabs plushie", 999.99, 1) },
+                {"006", ("skinamarink poster", 19.99, 7) },
+                {"007", ("zaxbys nibbler meal", 8.99, 22) },
+                {"008", ("snoop dogg cookbook", 23.99, 10) },
+                {"009", ("magic mike movie ticket (premium experience)", 299.99, 1) },
+                {"010", ("capgemini fast pass", 10000, 1) },
+                {"011", ("florida gators calculator", 99.99, 2) },
+                {"012", ("haydens room at cayce cove 222", 3.99, 1) }
             };
 
             carts = new Dictionary<string, List<string>>
             {
-                { "1e9d4ff6-22ee-4b4b-bd24-741afa04bf06", new List<string> { "001", "002" } }
+                { "1e9d4ff6-22ee-4b4b-bd24-741afa04bf06", new List<string> { "005", "008" } }
             };
         }
         public Task addToCart(string cartID, string itemID, int quantity)
