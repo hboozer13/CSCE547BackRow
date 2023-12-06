bdde3baddfa366634fd6d53ba2db6b3e37ed2862
Hayden Boozer <boozerhaydens@gmail.com>
2023-12-06 00:21:24 -0500

    checkout service

---
+++ BackRowStore/Services/CheckoutService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using BackRowStore;
+using Newtonsoft.Json;
+public class CheckoutService
+{
+    private readonly BackRowDbContext _context;
+
+    public CheckoutService(BackRowDbContext context)
+    {
+        _context = context;
+    }
+}
